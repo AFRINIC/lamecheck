d62f536676dd361ce4affa91ffebf852fe0a9828
Daniel Shaw <daniel@afrinic.net>
2017-05-12 09:40:25 +0400

    make wget quiet

--- get-zones.sh
+++ get-zones.sh
@@ -4,6 +4,6 @@
 # stuff is hard-coded. that's the way it is :)
 # expectation of `wget` in $PATH
 
-wget -e robots=off --wait 1 -r http://ftp.afrinic.net/pub/zones/
+wget -e robots=off --wait 1 -q -r http://ftp.afrinic.net/pub/zones/
 mv ftp.afrinic.net/pub/zones/*.arpa-AFRINIC zones/
 rm -r ftp.afrinic.net
